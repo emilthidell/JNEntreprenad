9414b28493d83e07ed89c53304287d867498d94d
RUdown <mail@emilthidell.se>
2025-10-22 14:34:57 +0200

    instagram embedd

--- src/components/Instagram.tsx
+++ src/components/Instagram.tsx
@@ -7,7 +7,7 @@ const Instagram = () => {
         <p className="text-sm">
           Instagram Feed
         </p>
-         
+         <InstagramFeed token="your_access_token"  counter="6"/> 
       </div>
     </footer>
   );
