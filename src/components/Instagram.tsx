3dafbf1255d4d26ec5b6b60a403730b403b47ff5
RUdown <mail@emilthidell.se>
2025-10-22 19:27:59 +0200

    contact

--- src/components/Instagram.tsx
+++ src/components/Instagram.tsx
@@ -4,7 +4,7 @@ const Instagram = () => {
   return (
     <footer className="bg-primary text-primary-foreground py-8">
       <div className="container mx-auto px-4 text-center">
-          <iframe onload="" src="https://442ee0d83cf24f0e8c11412acc591fc7.elf.site" style={{border:'none',width:'100%'}}></iframe>
+          <iframe onload="" src="https://07c8b8c24d9b40528b91742be7171f33.elf.site" style={{border:'none',width:'100%'}}></iframe>
       </div>
     </footer>
   );
