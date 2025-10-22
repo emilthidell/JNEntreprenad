8de99722c20cc1dececc2912212f9b135e0d946b
RUdown <mail@emilthidell.se>
2025-10-22 14:25:16 +0200

    instagram embedd

--- src/components/Instagram.tsx
+++ src/components/Instagram.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
-import InstagramFeed  from 'react-instagram-nb'
-import 'react-instagram-nb/dist/index.css'
+
 
 const Instagram = () => {
   return (
@@ -9,7 +7,7 @@ const Instagram = () => {
         <p className="text-sm">
           Instagram Feed
         </p>
-        <InstagramFeed token="your_access_token"  counter="6"/>  
+         
       </div>
     </footer>
   );
