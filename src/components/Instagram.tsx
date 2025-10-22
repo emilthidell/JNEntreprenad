c2e52361a5fc0c5406937123b69848fcbf7c5c58
RUdown <mail@emilthidell.se>
2025-10-22 20:19:52 +0200

    contact

--- src/components/Instagram.tsx
+++ src/components/Instagram.tsx
@@ -1,10 +1,9 @@
-import InstaFeed from '@/components/InstaFeed.jsx';
 
 const Instagram = () => {
   return (
     <footer className="bg-primary text-primary-foreground py-8">
       <div className="container mx-auto px-4 text-center">
-          <InstaFeed />
+          
       </div>
     </footer>
   );
